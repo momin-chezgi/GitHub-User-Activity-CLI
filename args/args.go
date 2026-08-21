e0499b47d7530aedd08b076b144fefda15238a5f
momin-chezgi <momin.chezgi@gmail.com>
2026-08-21 14:17:46 +0330

    Fixing the bug for the number of inputs at command and the returned name

--- args/args.go
+++ args/args.go
@@ -7,15 +7,15 @@ import (
 )
 
 func main() {
-	userName := flag.String("username", "momin-chezgi", "A valid user-name in GitHub")
+	flag.String("username", "momin-chezgi", "A valid user-name in GitHub")
 
 	flag.Parse()
 
-	if len(os.Args) > 3 {
-		fmt.Fprintf(os.Stderr, "Too much arguments: needed 1, given %v\n", len(os.Args)-2)
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, "Too much arguments: needed 1, given %v\n", len(os.Args)-1)
 		os.Exit(1)
 	}
 
 	// This section is just for tests
-	fmt.Fprintf(os.Stdout, "%v\n", *userName)
+	fmt.Fprintf(os.Stdout, "%v\n", os.Args[1])
 }
