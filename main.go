a46aecec713ec042f3fd90d92d894a93fb1e64ba
momin-chezgi <momin.chezgi@gmail.com>
2026-09-10 16:04:50 +0330

    More flexible for the considered input arguments

--- main.go
+++ main.go
@@ -5,16 +5,18 @@ import (
 	"os"
 )
 
+const neededArgs = 1
+
 func main() {
 
-	if len(os.Args) != 2 {
-		if len(os.Args) > 2 {
-			fmt.Fprintf(os.Stderr, "Too much arguments: needed 1, given %v\n", len(os.Args)-1)
+	if len(os.Args) != neededArgs+1 {
+		if len(os.Args) > neededArgs+1 {
+			fmt.Fprintf(os.Stderr, "Too much arguments: needed %v, given %v\n",neededArgs, len(os.Args)-1)
 		} else {
-			fmt.Fprintf(os.Stderr, "Too few arguments: needed 1, given %v\n", len(os.Args)-1)
+			fmt.Fprintf(os.Stderr, "Too few arguments: needed %v, given %v\n",neededArgs, len(os.Args)-1)
 		}
 		os.Exit(1)
 	}
 
-	fmt.Fprintf(os.Stdout, "%v\n", os.Args[1])
+	fmt.Fprintf(os.Stdout, "%v\n", os.Args[neededArgs])
 }
