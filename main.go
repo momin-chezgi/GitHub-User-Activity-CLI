f00c8257abb2fe6a7935251472968d02cbc01493
Mohammad Amin Chezgi <momin.chezgi@gmail.com>
2026-09-11 16:04:59 +0330

    Connection status handling and better error handling

--- main.go
+++ main.go
@@ -14,17 +14,36 @@ func main() {
 	user, err := userName()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
-	}
-	url := urlMaker(user)
-	req, err := http.NewRequest("GET", url, nil)
-	req.Header.Set("User-Agent", "github-user-activity")
-	resp, err := http.DefaultClient.Do(req)
-	defer resp.Body.Close()
-	bodyBytes, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf(err.Error())
 		os.Exit(1)
 	}
+
+	url := urlMaker(user)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+	}
+
+	req.Header.Set("User-Agent", "github-user-activity")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil || resp == nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	defer resp.Body.Close()
+
+	if connStat := checkStat(resp.StatusCode); connStat != nil {
+		fmt.Fprintf(os.Stderr, connStat.Error())
+		os.Exit(1)
+	}
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
 	fmt.Printf(string(bodyBytes))
 }
 
@@ -40,6 +59,22 @@ func userName() (string, error) {
 		return "", err
 	}
 	return os.Args[neededArgs], nil
+}
+
+func checkStat(sc int) error {
+	sc /= 100
+	switch sc {
+	case 1:
+		return errors.New("Informational error")
+	case 3:
+		return errors.New("Redirecting")
+	case 4:
+		return errors.New("Client error")
+	case 5:
+		return errors.New("Server error")
+	default:
+		return nil
+	}
 }
 
 func urlMaker(userName string) string {
