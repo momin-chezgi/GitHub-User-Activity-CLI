ed832f44bbed8c4656ff5adf1a49597e42f60963
Mohammad Amin Chezgi <momin.chezgi@gmail.com>
2026-09-11 13:18:28 +0330

    Phase1: Sending a HTTP request and recieving a REST (now without error handling)

--- main.go
+++ main.go
@@ -1,22 +1,47 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
 const neededArgs = 1
 
 func main() {
-
-	if len(os.Args) != neededArgs+1 {
-		if len(os.Args) > neededArgs+1 {
-			fmt.Fprintf(os.Stderr, "Too much arguments: needed %v, given %v\n",neededArgs, len(os.Args)-1)
-		} else {
-			fmt.Fprintf(os.Stderr, "Too few arguments: needed %v, given %v\n",neededArgs, len(os.Args)-1)
-		}
+	user, err := userName()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+	}
+	url := urlMaker(user)
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Set("User-Agent", "github-user-activity")
+	resp, err := http.DefaultClient.Do(req)
+	defer resp.Body.Close()
+	bodyBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf(err.Error())
 		os.Exit(1)
 	}
+	fmt.Printf(string(bodyBytes))
+}
 
-	fmt.Fprintf(os.Stdout, "%v\n", os.Args[neededArgs])
+func userName() (string, error) {
+	if len(os.Args) != neededArgs+1 {
+		var errMessage string
+		if len(os.Args) > neededArgs+1 {
+			errMessage = fmt.Sprintf("Too much arguments: needed %v, given %v\n", neededArgs, len(os.Args)-1)
+		} else {
+			errMessage = fmt.Sprintf("Too few arguments: needed %v, given %v\n", neededArgs, len(os.Args)-1)
+		}
+		err := errors.New(errMessage)
+		return "", err
+	}
+	return os.Args[neededArgs], nil
+}
+
+func urlMaker(userName string) string {
+	return fmt.Sprintf("https://api.github.com/users/%v/events", userName)
 }
