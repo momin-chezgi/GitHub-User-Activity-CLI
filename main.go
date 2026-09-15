b9308b1aaacdc36c31690c121eaef93806765670
momin-chezgi <momin.chezgi@gmail.com>
2026-09-15 13:51:08 +0330

    Rate limit handling

--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 const neededArgs = 1
@@ -15,7 +17,6 @@ const neededArgs = 1
 type Event struct {
 	Type      string  `json:"type"`
 	Repo      Repo    `json:"repo"`
-	Actor     Actor   `json:"actor"`
 	CreatedAt string  `json:"created_at"`
 	Payload   Payload `json:"payload"`
 }
@@ -30,10 +31,6 @@ type Payload struct {
 	RefType string `json:"ref_type"`
 }
 
-type Actor struct {
-	Login string `json:"login"`
-}
-
 func main() {
 	user, err := userName()
 	if err != nil {
@@ -41,7 +38,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	bodyBytes, err := getActivity(user)
+	bodyBytes, header, err := getActivity(user)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
 		os.Exit(1)
@@ -49,11 +46,27 @@ func main() {
 
 	events := make([]Event, 0)
 	if err := json.Unmarshal(bodyBytes, &events); err != nil {
-		fmt.Fprintf(os.Stderr, err.Error())
+		fmt.Fprintf(os.Stderr, "Err: Cannot parse the JSON body")
 		os.Exit(1)
 	}
 
 	fmt.Printf(eventSprinter(events))
+	if len(events) == 0 {
+		if header.Get("X-RateLimit-Remaining") == "" {
+			minutesLeft, err := calNextReset(header.Get("X-RateLimit-Reset"))
+			if err != nil {
+				fmt.Fprintf(os.Stderr, err.Error())
+				os.Exit(1)
+			}
+			fmt.Printf("GitHub rate limit is used up.")
+			if minutesLeft <= 1 {
+				fmt.Printf("Please wait a minute.\n")
+			} else {
+				fmt.Printf("You can wait till %v minutes from now to send the next request\n", minutesLeft)
+			}
+		}
+		fmt.Println("No action has been made recently")
+	}
 }
 
 func userName() (string, error) {
@@ -69,31 +82,31 @@ func userName() (string, error) {
 	return os.Args[neededArgs], nil
 }
 
-func getActivity(userName string) ([]byte, error) {
+func getActivity(userName string) ([]byte, http.Header, error) {
 	url := urlMaker(userName)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return nil, errors.New("Failed to send the request!\n")
+		return nil, nil, errors.New("Failed to make the request!\n")
 	}
 
 	req.Header.Set("User-Agent", "github-user-activity")
 
 	resp, err := http.DefaultClient.Do(req)
 	if resp == nil {
-		return nil, errors.New("Connection failed!\n")
+		return nil, resp.Header, errors.New("Connection failed!\n")
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return nil, errors.New(fmt.Sprintf("An error occurred with the status code %v\n", resp.StatusCode))
+		return nil, resp.Header, errors.New(fmt.Sprintf("An error occurred with the status code %v\n", resp.StatusCode))
 	}
 
 	out, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, errors.New("An error occurred at reading the response body!\n")
+		return nil, resp.Header, errors.New("An error occurred at reading the response body!\n")
 	}
-	return out, nil
+	return out, resp.Header, nil
 }
 
 func eventSprinter(events []Event) string {
@@ -132,4 +145,15 @@ func capitalise(s string) string {
 		return s
 	}
 	return strings.ToUpper(s[:1]) + s[1:]
+}
+
+func calNextReset(resetStr string) (int, error) {
+	nextReset, err := strconv.ParseInt(resetStr, 10, 64)
+	if err != nil {
+		return -1, errors.New("Cannot extract the reset time from the header\n")
+	}
+	now := time.Now().Unix()
+	secondsLeft := nextReset - now
+	return int((secondsLeft + 59) / 60), nil
+
 }
